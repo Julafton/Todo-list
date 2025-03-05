48effe4436c75feed9deaecdbfd197b14a899e32
Julafton <allapan2009@gmail.com>
2025-03-05 20:07:25 +0100

    New dashbord
    New admin dashbord

--- frontend/dashbord/index.js
+++ frontend/dashbord/index.js
@@ -1,5 +1,41 @@
 let usercount = 0;
 
+class userclass {
+    constructor(name) {
+        this.name = name;
+
+        let a = document.createElement("a");
+        a.textContent = name;
+
+        let br = document.createElement("br");
+        let hr = document.createElement("hr");
+
+        let usersdiv = document.querySelector(".users");
+
+        a.addEventListener("click", () => {
+            usercount--;
+            document.getElementById("usersH1").textContent = `Registered users: ${usercount}`;
+            a.style.display = "none";
+            br.style.display = "none";
+            hr.style.display = "none";
+
+            fetch("/api/Deleteuser", {
+                method: "POST",
+                "Content-Type": "Application/Json",
+                body: JSON.stringify({
+                    "username": localStorage.getItem("username"),
+                    "token": localStorage.getItem("token"),
+                    "user": this.name,
+                })
+            });
+        });
+
+        usersdiv.appendChild(a);
+        usersdiv.appendChild(br);
+        usersdiv.appendChild(hr);
+    }
+}
+
 window.onload = () => {
     let username = localStorage.getItem("username");
 
@@ -15,18 +51,8 @@ window.onload = () => {
                 usercount = data.length;
                 document.getElementById("usersH1").textContent = `Registered users: ${usercount}`;
 
-                let usersdiv = document.querySelector(".users");
-
                 data.forEach((user) => {
-                    let a = document.createElement("a");
-                    a.textContent = user;
-
-                    a.addEventListener("click", () => {
-                        usercount--;
-                        document.querySelector(this).style.display = "none";
-                    });
-
-                    usersdiv.appendChild(a);
+                    new userclass(user);
                 });
             });
         }
